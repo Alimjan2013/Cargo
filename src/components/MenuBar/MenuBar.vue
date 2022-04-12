991e921a2adb5b2a9ff8a644aad3e91fd49ca21c
Alimjan2013 <alimujiang1414@qq.com>
2022-04-12 11:52:15 +0800

    新增 侧目录

---
+++ src/components/MenuBar/MenuBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col bg-Background-0 h-full px-2 py-2">
+    <MenuItem />
+    <MenuItem />
+    <MenuItem />
+  </div>
+</template>
+<script>
+import MenuItem from "./MenuItem.vue";
+export default {
+  components: {
+    MenuItem,
+  },
+};
+</script>
---
+++ src/components/MenuBar/MenuItem.vue
@@ -0,0 +1,3 @@
+<template>
+  <a class="text-Body3 text-OpacityWhite-3 px-2 py-1" href="">是我item</a>
+</template>
