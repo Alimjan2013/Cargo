20437c8cbd7f37f2c0628805e1adf146c87a54db
Alimjan2013 <alimujiang1414@qq.com>
2022-04-12 09:50:30 +0800

    完成AppBar

---
+++ src/components/AppBar/AppBar.vue
@@ -0,0 +1,212 @@
+<script></script>
+
+<template>
+  <div class="bg-Background-3 w-full flex p-2">
+    <svg
+      width="2em"
+      height="2em"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="flex-1 mx-auto"
+    >
+      <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+      <path
+        d="M24 7C12.9543 7 4 15.9543 4 27C4 32.2301 5.71344 37.4362 9 41H39C42.2866 37.4362 44 32.2301 44 27C44 15.9543 35.0457 7 24 7Z"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <circle
+        cx="24"
+        cy="30"
+        r="4"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M24 20V26"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M24 12V14"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9 28H11"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13 18L14.4142 19.4142"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M37 28H39"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M34 19.4142L35.4142 18"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <svg
+      width="2em"
+      height="2em"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="flex-1 mx-auto"
+    >
+      <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M13.5 32C14.8807 32 16 30.8807 16 29.5C16 28.1193 14.8807 27 13.5 27C12.1193 27 11 28.1193 11 29.5C11 30.8807 12.1193 32 13.5 32Z"
+        fill="#f1f1f1"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M34.5 32C35.8807 32 37 30.8807 37 29.5C37 28.1193 35.8807 27 34.5 27C33.1193 27 32 28.1193 32 29.5C32 30.8807 33.1193 32 34.5 32Z"
+        fill="#f1f1f1"
+      />
+      <path
+        d="M7 37C5.34315 37 4 35.6569 4 34L4 24.7097C4 22.4363 5.28486 20.3581 7.3186 19.3422L8.00053 19.0015L10.3105 9.09194C10.7326 7.28117 12.3467 6 14.206 6L33.8943 6C35.7675 6 37.3899 7.29998 37.7981 9.12816L40.0031 19.0015L40.6834 19.3416C42.716 20.358 44 22.4355 44 24.7081V34C44 35.6569 42.6569 37 41 37H39.0031V38C39.0031 40.2091 37.2107 42 35.0016 42C32.7924 42 31 40.2091 31 38V37H17V38.0003C17 40.2093 15.2093 42 13.0003 42C10.7913 42 9.00053 40.2093 9.00053 38.0003V37H7Z"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14 22H34L32.348 14.5661C32.1447 13.6511 31.3331 13 30.3957 13L17.6043 13C16.6669 13 15.8553 13.6511 15.652 14.5661L14 22Z"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <svg
+      width="2em"
+      height="2em"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="flex-1 mx-auto"
+    >
+      <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+      <path
+        d="M44 44V20L24 4L4 20L4 44H16V26H32V44H44Z"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M24 44V34"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <svg
+      width="2em"
+      height="2em"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="flex-1 mx-auto"
+    >
+      <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+      <rect
+        x="4"
+        y="8"
+        width="40"
+        height="20"
+        rx="2"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <rect
+        x="12"
+        y="20"
+        width="24"
+        height="8"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M32 14H36"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M24 34V40"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16 36V38"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M32 36V38"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <svg
+      width="2em"
+      height="2em"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="mx-auto flex-1"
+    >
+      <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+      <path
+        d="M24.5 4L9 44L24.5 34.9091L40 44L24.5 4Z"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,64 @@
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
-    extend: {},
+    spacing: {
+      0: "4px",
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+      7: "64px",
+      8: "96px",
+    },
+    fontSize: {
+      Display1: "3.2rem",
+      Display2: "2.75rem",
+      Display3: "2.25rem",
+      Body1: "2rem",
+      Body2: "1.75rem",
+      Body3: "1.5rem",
+      Sub1: "1.4rem",
+      Sub2: "1.25rem",
+      Sub3: "1.125rem",
+    },
+    screens: {
+      Standard: "690px",
+      // => @media (min-width: 690px) { ... }
+
+      Wide: "930px",
+      // => @media (min-width: 930px) { ... }
+
+      ExtraWide: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      SuperWide: "1920px",
+      // => @media (min-width: 1920px) { ... }
+    },
+    colors: {
+      Background: {
+        0: "#000000",
+        1: "#17181B",
+        2: "#202124",
+        3: "#282A2D",
+      },
+      OpacityBlack: {
+        1: "rgba(0, 0, 0, 0.84)",
+        2: "rgba(0, 0, 0, 0.78)",
+        3: "rgba(0, 0, 0, 0.6)",
+      },
+      OpacityWhite: {
+        1: "#FFFFFF",
+        2: "rgba(255, 255, 255, 0.72)",
+        3: "rgba(255, 255, 255, 0.56)",
+      },
+      Line: {
+        1: "rgba(255, 255, 255, 0.46)",
+        2: "rgba(255, 255, 255, 0.24)",
+      },
+      // ...
+    },
   },
   plugins: [],
 };
