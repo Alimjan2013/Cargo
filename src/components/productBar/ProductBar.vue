ea1f52f20243dcf818ee53e8b12de3ee34fd7434
Alimjan2013 <alimujiang1414@qq.com>
2022-04-12 11:06:46 +0800

    完成 productBar 的框架

---
+++ src/components/productBar/ProductBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex-1 flex flex-col space-y-5">
+    <div class="text-Sub1 text-OpacityWhite-2">系列一</div>
+    <div class="flex space-x-4 overflow-x-auto">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductCard from "./ProductCard.vue";
+
+export default {
+  components: {
+    ProductCard,
+  },
+};
+</script>
+
+<style scoped></style>
