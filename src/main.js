238675d560b060925312e8cc9b7bb7bbf4dfbb96
Alimjan2013 <alimujiang1414@qq.com>
2022-04-13 21:21:58 +0800

    从store 获取数据并解析

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import appRouter from "./app.router.ts";
+import appStore from "./app.store";
+/**
+ * 创建应用
+ */
 const app = createApp(App);
+
 app.use(appRouter);
+app.use(appStore);
+
+/**
+ * 卸载应用
+ */
 app.mount("#app");
--- src/productDetails/ProductDetails.vue
+++ src/productDetails/ProductDetails.vue
@@ -1,5 +1,9 @@
 <template>
-  <div><p>heheh</p></div>
+  <div class="text-OpacityWhite-1">
+    <p>{{ product.name }}</p>
+    <div>{{ product.introduction }}</div>
+  </div>
+  <img v-bind:src="product.detailsImages[0]" alt="" />
 </template>
 <script>
 export default {
@@ -7,16 +11,26 @@ export default {
     return {
       product: {},
       productID: "",
+      catalogue: "",
     };
   },
   methods: {
-    // getProduct(){
-    // },
+    getProduct(productID, catalogue) {
+      const productList = this.$store.state.productList.filter(
+        (item) => item.catalogue === catalogue
+      );
+      this.product = productList[0].product.filter(
+        (item) => item._id === this.productID
+      )[0];
+      console.log(this.product);
+    },
   },
   created() {
     this.productID = this.$route.params.productID;
-
+    this.catalogue = this.$route.params.secondCatalogue;
+    this.getProduct(1, this.catalogue);
     console.log(this.$route.params.productID);
+    console.log(this.$route.params.secondCatalogue);
   },
 };
 </script>
