c6684832d1a8dd06b44f28d15694b08bdad83106
Alimjan2013 <alimujiang1414@qq.com>
2022-04-28 14:43:34 +0800

    新增搜索框

--- src/components/MenuBar/MenuBar.vue
+++ src/components/MenuBar/MenuBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex items-center bg-Background-1">
-    <div class="flex flex-1 px-3 Wide:px-[80px] py-2 Wide:space-x-6 space-x-2">
+  <div class="flex px-3 Wide:px-[80px] items-center bg-Background-1">
+    <div class="flex flex-1 py-2 Wide:space-x-6 space-x-2">
       <MenuItem
         v-for="name of MenuItemList"
         :key="name._id"
@@ -8,9 +8,48 @@
         @click="handleClick(name.name)"
       />
     </div>
+    <div class="Wide:flex bg-Background-3 py-0 pl-1 hidden">
+      <input
+        type="text"
+        class="placeholder:px-0 focus:border-Line-2 focus:ring-1 focus:outline-none p-0 focus:ring-Line-2"
+        placeholder="请输入搜索内容"
+      />
+      <div class="px-1 py-0">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 48 48"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="48" height="48" fill="white" fill-opacity="0.01" />
+          <path
+            d="M21 38C30.3888 38 38 30.3888 38 21C38 11.6112 30.3888 4 21 4C11.6112 4 4 11.6112 4 21C4 30.3888 11.6112 38 21 38Z"
+            fill="none"
+            stroke="#f1f1f1"
+            stroke-width="4"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M26.6568 14.3431C25.2091 12.8954 23.2091 12 21 12C18.7909 12 16.7909 12.8954 15.3431 14.3431"
+            stroke="#f1f1f1"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M33.2218 33.2218L41.7071 41.7071"
+            stroke="#f1f1f1"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+    </div>
 
     <router-link
-      class="Wide:text-Body3 text-Sub2 px-2 py-1 text-OpacityWhite-3"
+      class="Wide:text-Sub1 text-Sub3 px-2 py-1 text-OpacityWhite-3"
       to="/store/aboutme"
     >
       <svg
