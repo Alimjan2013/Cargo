217828c9cb644f85f74ad0703d812874494dff3c
Alimjan2013 <alimujiang1414@qq.com>
2022-04-29 21:54:32 +0800

    新增首页推荐卡片组件

---
+++ src/components/RecommendCard/RecommendCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bg-Background-3 flex-1 text-OpacityWhite-2 p-3 space-y-2">
+    <div class="Wide:text-Body3 text-Sub2">{{ cardName }}</div>
+    <div v-if="productList.length === 0">加载中</div>
+    <div v-else class="flex">
+      <div
+        v-for="item in productList"
+        :key="item"
+        class="flex flex-1 flex-col items-center space-y-1"
+      >
+        <img
+          class="ExtraWide:w-8 ExtraWide:h-8 Wide:w-7 Wide:h-7 w-6 h-6 rounded-2xl"
+          :src="item.cover"
+          alt=""
+        />
+        <p class="ExtraWide:text-Sub1 text-Sub3 truncate">
+          {{ item.name }}
+        </p>
+        <button
+          class="ExtraWide:px-3 ExtraWide:py-1 px-1 py-0 bg-OpacityWhite-1 text-Background-2"
+        >
+          预览
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardName: {
+      type: String,
+      default: "热门推荐",
+    },
+    productList: {
+      type: Array,
+      default: [],
+    },
+  },
+  components: {},
+  created() {},
+
+  computed: {},
+};
+</script>
--- src/index/Index.vue
+++ src/index/Index.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="flex-1 bg-Background-1 p-3">
-    <Swiper />
+  <div class="space-y-3">
+    <div class="flex-1 bg-Background-1 p-3">
+      <Swiper />
+    </div>
+    <div
+      class="flex-1 bg-Background-1 p-3 Wide:flex Wide:space-x-3 Wide:space-y-[0] space-y-2"
+    >
+      <RecommendCard :productList="RecommendProduct" cardName="热门推荐" />
+      <RecommendCard :productList="NewstProduct" cardName="最近使用" />
+    </div>
   </div>
 </template>
 
 <script>
 import Swiper from "../components/Swiper.vue";
+import RecommendCard from "../components/RecommendCard/RecommendCard.vue";
 export default {
+  data() {
+    return {
+      RecommendProduct: [],
+      NewstProduct: [],
+    };
+  },
   components: {
     Swiper,
+    RecommendCard,
   },
   created() {
     console.log("我在这里");
+    this.findRecommendProduct();
+    this.findNewstProduct();
     // this.findCatalogue();
+  },
+  methods: {
+    findRecommendProduct() {
+      fetch(
+        "https://d40d2143-72af-4a3b-b428-2874934cbc4f.bspapp.com/recommendProduct",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ recommendType: "global", quantity: 4 }),
+        }
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
+          this.RecommendProduct = json;
+        });
+    },
+    findNewstProduct() {
+      fetch(
+        "https://d40d2143-72af-4a3b-b428-2874934cbc4f.bspapp.com/recommendProduct",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ recommendType: "newst", quantity: 4 }),
+        }
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
+          this.NewstProduct = json;
+        });
+    },
   },
   computed: {},
 };
