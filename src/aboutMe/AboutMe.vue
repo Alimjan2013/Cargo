edcee21e83d2e588061b3a053fbd37c36315a59a
Alimjan2013 <alimujiang1414@qq.com>
2022-05-16 09:56:54 +0800

    解决订单全屏滑动问题

--- src/aboutMe/AboutMe.vue
+++ src/aboutMe/AboutMe.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid gap-2 grid-cols-8 text-OpacityWhite-1">
+  <div class="grid gap-2 grid-cols-8 text-OpacityWhite-1 h-full">
     <div class="col-span-2">
       <div>个人中心</div>
       <div>领券中心</div>
     </div>
-    <div class="col-span-6"><OrderList /></div>
+    <div class="col-span-6 overflow-auto h-full"><OrderList /></div>
   </div>
 </template>
 
--- src/aboutMe/OrderList.vue
+++ src/aboutMe/OrderList.vue
@@ -1,51 +1,53 @@
 <template>
-  <div class="w-full px-2 py-16 sm:px-0">
+  <div class="w-full h-full px-2 py-16 sm:px-0">
     <TabGroup>
-      <div class="bg-Background-3 p-3">
-        <div class="text-Body3 mb-3">订单中心</div>
-        <TabList class="flex space-x-2 bg-Background-1 p-2">
-          <Tab
-            v-for="category in categoriesName"
-            as="template"
-            :key="category"
-            v-slot="{ selected }"
-          >
-            <button
-              :class="[
-                'w-full  py-1 text-Sub3 font-medium leading-5 ',
-                '',
-                selected
-                  ? 'shadow bg-Background-3 text-OpacityWhite-1'
-                  : ' text-OpacityWhite-3 hover:bg-Background-3 hover:text-OpacityWhite-2',
-              ]"
+      <div class="h-full flex flex-col">
+        <div class="bg-Background-3 p-3 row-span-2 col-span-2">
+          <div class="text-Body3 mb-3">订单中心</div>
+          <TabList class="flex space-x-2 bg-Background-1 p-2">
+            <Tab
+              v-for="category in categoriesName"
+              as="template"
+              :key="category"
+              v-slot="{ selected }"
             >
-              {{ category }}
-            </button>
-          </Tab>
-        </TabList>
-      </div>
+              <button
+                :class="[
+                  'w-full  py-1 text-Sub3 font-medium leading-5 ',
+                  '',
+                  selected
+                    ? 'shadow bg-Background-3 text-OpacityWhite-1'
+                    : ' text-OpacityWhite-3 hover:bg-Background-3 hover:text-OpacityWhite-2',
+                ]"
+              >
+                {{ category }}
+              </button>
+            </Tab>
+          </TabList>
+        </div>
 
-      <TabPanels class="mt-2 bg-Background-3">
-        <TabPanel
-          v-for="(posts, idx) in Object.values(categories)"
-          :key="idx"
-          :class="[
-            'rounded-xl  p-3 ',
-            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-          ]"
-        >
-          <ul class="space-y-1 overflow-auto overflow-y-auto h-full">
-            <p v-if="posts.length === 0">暂无内容</p>
-            <li
-              v-for="post in posts"
-              :key="post._id"
-              class="relative bg-Background-4 rounded-md p-3 hover:bg-gray-100 space-y-1"
-            >
-              <OrderCard :id="post._id" :catalogue="post.catalogue.second" />
-            </li>
-          </ul>
-        </TabPanel>
-      </TabPanels>
+        <TabPanels class="mt-2 bg-Background-3 flex-1 overflow-auto">
+          <TabPanel
+            v-for="(posts, idx) in Object.values(categories)"
+            :key="idx"
+            :class="[
+              'rounded-xl  p-3 overflow-auto  h-full',
+              'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+            ]"
+          >
+            <ul class="space-y-1">
+              <p v-if="posts.length === 0">暂无内容</p>
+              <li
+                v-for="post in posts"
+                :key="post._id"
+                class="relative bg-Background-4 rounded-md p-3 hover:bg-gray-100 space-y-1"
+              >
+                <OrderCard :id="post._id" :catalogue="post.catalogue.second" />
+              </li>
+            </ul>
+          </TabPanel>
+        </TabPanels>
+      </div>
     </TabGroup>
   </div>
 </template>
